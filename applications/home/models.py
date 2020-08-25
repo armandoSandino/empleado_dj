98469a6a09e610ad41eabe5a7c7fbd8645c03d95
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 11:22:13 -0600

    chore: add model and register in the  "admin"

--- applications/home/models.py
+++ applications/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Prueba(models.Model):
+    titulo = models.CharField( max_length=100, default='sin definir')
+    subtitulo =  models.CharField(max_length=50)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return self.titulo

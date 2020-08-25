9113d4f7cc93c790f692346a09417430463dd179
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 14:00:38 -0600

    chore: add model used

--- applications/persona/models.py
+++ applications/persona/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from applications.departamento.models import Departamento
+
+class Empleado(models.Model):
+    """ Modelo para tabla  empleado """
+
+    JOB_CHOICES = (
+        ('0', 'CONTADOR'),
+        ('1', 'ADMINISTRADOR'),
+        ('2', 'ECONOMISTA'),
+        ('3', 'OTRO')
+    )
+
+    first_name = models.CharField('Nombres', max_length=60, blank=False)
+    last_name = models.CharField('Apellidos', max_length=60, blank=False)
+    job = models.CharField('Trabajo', max_length=50, choices=JOB_CHOICES)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    # image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+
+    def __str__(self):
+        return str(self.id) + '-' + self.first_name + '-' + self.last_name

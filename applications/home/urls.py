9a2304ce1935c6ba4a2790dcb48ff85cde63946b
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 10:32:40 -0600

    chore: add ListView

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.PruebaView.as_view() ) 
+    path('home/', views.PruebaView.as_view() ) ,
+    path('lista/', views.PruebaListView.as_view())
 ]
--- applications/home/views.py
+++ applications/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 #
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 # vistas basadas en clases, nos permiten utilizar vistas genericas como
 """
@@ -11,3 +11,9 @@ UpdateView
 """
 class PruebaView(TemplateView):
     template_name = 'home/home.html'
+
+
+class PruebaListView(ListView):
+    template_name = 'home/lista.html'
+    context_object_name = 'listaNumero'
+    queryset = ['one', 'two', 'three',4,5]

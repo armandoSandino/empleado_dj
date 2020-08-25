3899e05971d9d1a78185fcc39682aa2bec6d0ff8
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 10:18:39 -0600

    chore: ad configuration for different environments, add unipath to manage view directories

---
+++ empleado/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.child('db.sqlite3')
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'

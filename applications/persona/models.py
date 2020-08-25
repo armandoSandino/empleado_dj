7c6c63db43a6027a6542f86f3f696bfd76169108
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 16:14:35 -0600

    chore: add class Meta for the model used

--- applications/persona/models.py
+++ applications/persona/models.py
@@ -18,5 +18,11 @@ class Empleado(models.Model):
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     # image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
 
+    class Meta:
+        verbose_name = 'Mi Empleado'
+        verbose_name_plural = 'All employees'
+        ordering = ['last_name']
+
+
     def __str__(self):
         return str(self.id) + '-' + self.first_name + '-' + self.last_name

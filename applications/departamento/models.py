b38477118b64603689356fa153a79b55a66c79cb
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 12:39:30 -0600

    chore: add model department

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Departamento(models.Model):
+    name = models.CharField('Nombre', max_length=50, default='', blank= False)
+    short_name = models.CharField('Nombre corto', max_length=20, default='', blank=False)
+    anulate = models.BooleanField('Anulado', default=False, blank=False)
+
+    def __str__(self):
+        return self.name

317ecdacbe25c9f9f8c90e3f02408bc0279776da
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 15:54:51 -0600

    chore: add class meta fot the model department

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 class Departamento(models.Model):
     name = models.CharField('Nombre', max_length=50, default='', blank= False)
-    short_name = models.CharField('Nombre corto', max_length=20, default='', blank=False)
+    short_name = models.CharField('Nombre corto', max_length=20, default='',unique=True, blank=False)
     anulate = models.BooleanField('Anulado', default=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Mi Departamento'
+        verbose_name_plural = 'All department'
+        ordering = ['-name']
+        unique_together = ('name', 'short_name') 
 
     def __str__(self):
         return self.name

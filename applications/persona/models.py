4ac088ef50ad71ffbf52a99799464e3e2ca74573
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 18:06:19 -0600

    chore:  add field for employee photo, also add skills model, added Pillow==7.2.0 package

--- applications/persona/models.py
+++ applications/persona/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 from applications.departamento.models import Departamento
 
+
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=50, default='Sin definir')
+
+    class Meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades del empleado'
+
+    def __str__(self):
+        return str(self.id) + '-' + self.habilidad
+
 class Empleado(models.Model):
     """ Modelo para tabla  empleado """
 
@@ -16,13 +27,13 @@ class Empleado(models.Model):
     last_name = models.CharField('Apellidos', max_length=60, blank=False)
     job = models.CharField('Trabajo', max_length=50, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    # image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+    avatar = models.ImageField( upload_to='empleado', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Mi Empleado'
         verbose_name_plural = 'All employees'
-        ordering = ['last_name']
-
+        ordering = ['last_name', 'last_name']
+        unique_together = ('first_name', 'departamento')
 
     def __str__(self):
         return str(self.id) + '-' + self.first_name + '-' + self.last_name

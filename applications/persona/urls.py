d9385426f9cd0362ed51369af9222c7b6b304588
Juan Sandino <jsandino@aimservices.tech>
2020-08-26 11:15:34 -0600

    refactor feat: modify route employee

--- applications/persona/urls.py
+++ applications/persona/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('listar-todo-empleados/', views.ListAllEmplados.as_view() ),
     # Esta ruta es equivalente a la posterior
     # path('lista-by-area/<termino>/', views.ListByAreaEmpleado.as_view())
-    path('lista-by-area/<str:termino>', views.ListByAreaEmpleado.as_view())
+    path('lista-by-area/<str:termino>/', views.ListByAreaEmpleado.as_view())
 ]

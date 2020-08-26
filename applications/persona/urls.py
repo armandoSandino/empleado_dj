ac26e016d09f6c83f4da12dc56d5d61178c37d6b
Juan Sandino <jsandino@aimservices.tech>
2020-08-26 10:53:17 -0600

    feat: add employee list by department

--- applications/persona/urls.py
+++ applications/persona/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listar-todo-empleados/', views.ListAllEmplados.as_view() )
+    path('listar-todo-empleados/', views.ListAllEmplados.as_view() ),
+    path('lista-by-area/', views.ListByAreaEmpleado.as_view())
 ]
--- applications/persona/views.py
+++ applications/persona/views.py
@@ -9,3 +9,10 @@ class ListAllEmplados(ListView):
     # puede acceder a los datos del modelo mediante 'context_object_name' o 'object_list'
     # context_object_name = 'data'
     
+class  ListByAreaEmpleado(ListView):
+    """ Listar todos los empleados de un area de la empresa """
+    template_name = 'persona/list_all.html' 
+    # filtrar empleado por departamento
+    queryset = Empleado.objects.filter(
+        departamento__short_name ='AC'
+    ) 

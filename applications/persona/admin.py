14e90374b5b1be85cb1381bc74a8abecd82bb260
Juan Sandino <jsandino@aimservices.tech>
2020-08-25 20:45:45 -0600

    refactor chore: record model activities and customize the way records are displayed in the administrator

--- applications/persona/admin.py
+++ applications/persona/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Empleado
+from .models import Empleado, Habilidades
 
-admin.site.register(Empleado)
+
+admin.site.register(Habilidades)
+
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'job',
+        'departamento',
+    )
+
+admin.site.register(Empleado, EmpleadoAdmin)

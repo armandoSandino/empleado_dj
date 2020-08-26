c900f87d13d2768f3e3c2ff1f6e8603af3755ac4
Juan Sandino <jsandino@aimservices.tech>
2020-08-26 09:10:10 -0600

    refactor chore: customize the management of the Person/Employee model in the administrator

--- applications/persona/admin.py
+++ applications/persona/admin.py
@@ -5,11 +5,25 @@ from .models import Empleado, Habilidades
 admin.site.register(Habilidades)
 
 class EmpleadoAdmin(admin.ModelAdmin):
+    # lista de campos a mostrar en el administrador
     list_display = (
         'first_name',
         'last_name',
         'job',
         'departamento',
+        'full_name',
     )
+    # agregar valor al campo que no existe en mi modelo 'full_name'
+    def full_name(self, obj ):
+        #operaciones necesarias
+        
+        return obj.first_name + ' ' + obj.last_name
+
+    # agregar/habilitar buscador, puede especificar laos campos por los que buscar
+    search_fields = ('first_name',)
+    # agregar/hablitar filtros para los campos
+    list_filter = ('job','habilidades',)
+    # agregar filtro horizontal unicamente para las relaciones many to many
+    filter_horizontal = ('habilidades',)
 
 admin.site.register(Empleado, EmpleadoAdmin)
